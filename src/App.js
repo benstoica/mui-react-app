b77a8d3abb7bd91495ada73ffe7f664c139882ee
Ben Stoica <bstoica@uwm.com>
2022-08-08 15:08:21 -0400

    creating layout and responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Stack } from "@mui/material";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <Box>
+      <Stack direction='row' spacing={2} justifyContent='space-between'>
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
+  );
 }
 
 export default App;
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+export const Rightbar = () => {
+  return (
+    <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+      Rightbar
+    </Box>
+  );
+};
+
+export default Rightbar;
