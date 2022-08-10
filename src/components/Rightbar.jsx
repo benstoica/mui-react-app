b34b5f8cdd6cd9752bb8f7252f93ef800600c370
Ben Stoica <bstoica@uwm.com>
2022-08-10 16:09:53 -0400

    completed rightbar

--- src/components/Rightbar.jsx
+++ src/components/Rightbar.jsx
@@ -1,14 +1,146 @@
 import React from "react";
-import { Box } from "@mui/material";
-import { Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  AvatarGroup,
+  Avatar,
+  ImageList,
+  ImageListItem,
+  ListItemText,
+  ListItemAvatar,
+  ListItem,
+  Divider,
+  List,
+} from "@mui/material";
 
 export const Rightbar = () => {
   return (
     <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
-      <Box positino='fixed'>
+      <Box positino='fixed' width={300}>
         <Typography variant='h6' fontWeight={100}>
           Olnine Friends
         </Typography>
+        <AvatarGroup max={7}>
+          <Avatar
+            alt='Remy Sharp'
+            src='https://material-ui.com/static/images/avatar/1.jpg'
+          />
+          <Avatar
+            alt='Travis Howard'
+            src='https://material-ui.com/static/images/avatar/2.jpg'
+          />
+          <Avatar
+            alt='Cindy Baker'
+            src='https://material-ui.com/static/images/avatar/3.jpg'
+          />
+          <Avatar
+            alt='Agnes Walker'
+            src='https://material-ui.com/static/images/avatar/4.jpg'
+          />
+          <Avatar
+            alt='Trevor Henderson'
+            src='https://material-ui.com/static/images/avatar/5.jpg'
+          />
+        </AvatarGroup>
+        <Typography variant='h6' fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src='https://material-ui.com/static/images/image-list/breakfast.jpg'
+              alt='breakfast and flowers on a tray'></img>
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src='https://material-ui.com/static/images/image-list/burgers.jpg'
+              alt=''
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src='https://material-ui.com/static/images/image-list/camera.jpg'
+              alt=''
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant='h6' fontWeight={100} mt={2} mb={2}>
+          Latest Conversations
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+          <ListItem alignItems='flex-start'>
+            <ListItemAvatar>
+              <Avatar
+                alt='Remy Sharp'
+                src='https://material-ui.com/static/images/avatar/1.jpg'
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary='Brunch this weekend?'
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component='span'
+                    variant='body2'
+                    color='text.primary'>
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant='inset' component='li' />
+          <ListItem alignItems='flex-start'>
+            <ListItemAvatar>
+              <Avatar
+                alt='Travis Howard'
+                src='https://material-ui.com/static/images/avatar/2.jpg'
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary='Summer BBQ'
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component='span'
+                    variant='body2'
+                    color='text.primary'>
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant='inset' component='li' />
+          <ListItem alignItems='flex-start'>
+            <ListItemAvatar>
+              <Avatar
+                alt='Cindy Baker'
+                src='https://material-ui.com/static/images/avatar/3.jpg'
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary='Oui Oui'
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component='span'
+                    variant='body2'
+                    color='text.primary'>
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
       </Box>
     </Box>
   );
